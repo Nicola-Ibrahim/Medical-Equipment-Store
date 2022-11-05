d698a54e6934f9126a30100f7e1cbe253cb8ed79
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-05 15:55:04 +0200

    Added a navigation endpoint to doctor app views

--- home/urls.py
+++ home/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/warehouse/", include("warehouse.urls")),
+    path("api/doctor/", include("doctor.urls")),
 ]
