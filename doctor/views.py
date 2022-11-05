31efda32e61592574bc612c6e0268c1ed07816f3
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-05 15:54:21 +0200

    doctor app views

--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Order 
+from .serializers import OrderSerializer
+from accounts.mixins import WarehousePermissionMixin
 
 # Create your views here.
+class OrdersListView(
+    ListCreateAPIView):
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderRetrieveView(
+    RetrieveUpdateDestroyAPIView):
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
