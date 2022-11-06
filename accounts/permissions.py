0b1c4b438aaee3251afd772349f914074e80baac
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-06 20:46:58 +0200

    permission method deleted

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,22 +1,15 @@
 from rest_framework import permissions
 
-class IsWarehousePermission(permissions.DjangoModelPermissions):
-
+class CustomDjangoModelPermission(permissions.DjangoModelPermissions):
     perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': [],
-        'HEAD': [],
-        'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
-    }
+            'GET': ['%(app_label)s.view_%(model_name)s'],
+            'OPTIONS': [],
+            'HEAD': [],
+            'POST': ['%(app_label)s.add_%(model_name)s'],
+            'PUT': ['%(app_label)s.change_%(model_name)s'],
+            'PATCH': ['%(app_label)s.change_%(model_name)s'],
+            'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        }
 
-    
 
-    def has_object_permission(self, request, view, obj):
-
-        # Check if the warehouse is author of a product
         
-        if(obj.warehouse == request.user):
-            return True
