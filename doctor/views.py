f4a2655ef6581c054e0831dd95858bc13dec16ae
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-06 20:50:27 +0200

    removed one of mixin package

--- doctor/views.py
+++ doctor/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Order 
 from .serializers import OrderSerializer
-from accounts.mixins import WarehousePermissionMixin
 
 # Create your views here.
 class OrdersListView(
