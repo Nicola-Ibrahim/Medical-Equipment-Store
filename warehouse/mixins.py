92bb2699c431040cec4215d1d3cc53e0ae916172
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-06 20:51:20 +0200

    Change the behavior of queryset method

--- warehouse/mixins.py
+++ warehouse/mixins.py
@@ -1,7 +1,18 @@
+from accounts.models import User
 
 class WarehouseQuerySetMixin:
     def get_queryset(self, *args, **kwargs):
-        lookup_data = dict()
+        """
+        Display only related products to warehouse
+        Display all products if the user is admin
+        """
+        
         qs = super().get_queryset(*args, **kwargs)
-        lookup_data['warehouse'] = self.request.user
-        return qs.filter(**lookup_data)
+
+        print(self.request.user)
+        if(self.request.user.type != User.Type.ADMIN):
+            lookup_data = dict()
+            lookup_data['warehouse'] = self.request.user
+            qs = qs.filter(**lookup_data)
+            
+        return qs
