381ddf7f2b6c21e0f36c24570d69d73dc6e7fce9
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-08 10:35:50 +0200

    Added a new url for warehouse's orders list

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import ProductsListView, ProductRetrieveView
+from .views import ProductsListView, ProductRetrieveView, WarehouseOrdersListView
 
 
 urlpatterns = [
     path("products_list/", ProductsListView.as_view(), name="products-list"),
     path("product_details/<slug:slug>", ProductRetrieveView.as_view(), name="product-detail"),
+    path("orders_list/", WarehouseOrdersListView.as_view(), name="orders-list"),
 ]
