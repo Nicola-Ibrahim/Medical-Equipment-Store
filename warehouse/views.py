cbb927efd7d0725e3a41843941c60bbc2339c518
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-08 10:36:26 +0200

    Added a new view for displaying warehouse's orders

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,14 +1,22 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from .models import Product 
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, WarehouseOrdersSerializer
+from .mixins import ProductQuerySetMixin, WarehouseOrdersQuerySetMixin
+
 from accounts.mixins import PermissionMixin
-from .mixins import WarehouseQuerySetMixin
+from doctor.models import OrderProduct
 
 # Create your views here.
 class ProductsListView(
     PermissionMixin,
-    WarehouseQuerySetMixin,
+    ProductQuerySetMixin,
     ListCreateAPIView):
+
+    """
+    Display all related products to specific warehouse
+    Create a new product instance
+    """
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -17,8 +25,29 @@ class ProductsListView(
 class ProductRetrieveView(
     PermissionMixin,
     RetrieveUpdateDestroyAPIView):
+
+    """
+    Update and delete a product related to a warehouse
+    """
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'slug'
 
 
+class WarehouseOrdersListView(
+    WarehouseOrdersQuerySetMixin,
+    ListCreateAPIView):
+
+    """
+    Obtain all related orders
+    -----
+    Make inner join between OrderProduct table 
+    and Product table where warehouse == current user
+    """
+
+    queryset = OrderProduct.objects.all()
+    serializer_class = WarehouseOrdersSerializer
+
+
+    
