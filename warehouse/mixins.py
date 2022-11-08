12c02524508153eacccaea2eb1b2a54dd73122cd
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-08 10:33:36 +0200

    Added a new queryset mixin to get warehouse's orders

--- warehouse/mixins.py
+++ warehouse/mixins.py
@@ -1,6 +1,7 @@
 from accounts.models import User
+from .models import Product
 
-class WarehouseQuerySetMixin:
+class ProductQuerySetMixin:
     def get_queryset(self, *args, **kwargs):
         """
         Display only related products to warehouse
@@ -9,10 +10,24 @@ class WarehouseQuerySetMixin:
         
         qs = super().get_queryset(*args, **kwargs)
 
-        print(self.request.user)
         if(self.request.user.type != User.Type.ADMIN):
             lookup_data = dict()
             lookup_data['warehouse'] = self.request.user
             qs = qs.filter(**lookup_data)
             
+        return qs
+    
+
+class WarehouseOrdersQuerySetMixin:
+    def get_queryset(self, *args, **kwargs):
+        """
+        Display only related orders to warehouse
+        Display all products if the user is admin
+        """
+        
+        qs = super().get_queryset(*args, **kwargs)
+        
+        warehouse_products_qs = Product.objects.filter(warehouse=self.request.user)
+        qs = qs.select_related('order').filter(product__in=warehouse_products_qs)
+
         return qs
