3b99a9695ad8594ff644414f4d3373203d510de1
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-10 17:03:01 +0200

    doctor app urls

--- doctor/urls.py
+++ doctor/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from .views import OrdersListView, OrderRetrieveView
 
+app_name='doctor'
 
 urlpatterns = [
     path("orders_list/", OrdersListView.as_view(), name="orders-list"),
-    path("order_details/<int:pk>", OrderRetrieveView.as_view(), name="order-detail"),
+    path("order_details/<int:pk>/", OrderRetrieveView.as_view(), name="order-details"),
 ]
