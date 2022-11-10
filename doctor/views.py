36c7e42cb3be4f50ec1f3b647fb0570bfc8a7975
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-10 17:03:20 +0200

    Added permission mixin on views

--- doctor/views.py
+++ doctor/views.py
@@ -1,16 +1,22 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Order 
 from .serializers import OrderSerializer
+from .mixin import OrderQuerySetMixin
+from accounts.mixins import PermissionMixin
 
 # Create your views here.
 class OrdersListView(
+    PermissionMixin,
+    OrderQuerySetMixin,
     ListCreateAPIView):
 
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 
+
 class OrderRetrieveView(
+    PermissionMixin,
     RetrieveUpdateDestroyAPIView):
 
     queryset = Order.objects.all()
