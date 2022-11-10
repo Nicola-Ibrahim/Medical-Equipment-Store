e67d155607b88cef8de66e1d960d45a1b961a7b4
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-10 21:02:05 +0200

    Added a new url for update an order's flag

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path
-from .views import ProductsListView, ProductRetrieveView, WarehouseOrdersListView
+from .views import ProductsListView, ProductRetrieveView, WarehouseOrdersListView, WarehouseOrdersUpdateView
 
+
+app_name = 'warehouse'
 
 urlpatterns = [
     path("products_list/", ProductsListView.as_view(), name="products-list"),
     path("product_details/<slug:slug>", ProductRetrieveView.as_view(), name="product-detail"),
     path("orders_list/", WarehouseOrdersListView.as_view(), name="orders-list"),
+    path("order_update/<int:pk>/", WarehouseOrdersUpdateView.as_view(), name="order-update"),
 ]
