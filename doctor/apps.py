af3b1a77a53fedf5e9770cba67ab4c267447aceb
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-12 16:46:28 +0200

    Read signal script file when starting the app

--- doctor/apps.py
+++ doctor/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DoctorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "doctor"
+
+    def ready(self) -> None:
+        from . import signals
+        return super().ready()
