d0135a2c7b167c6bc13fbe5f998443fc535e5424
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-12 12:49:49 +0200

    Added method for calculating the price

--- doctor/models.py
+++ doctor/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from accounts.models import Doctor, DeliveryWorker
 from warehouse.models import Product
-from .utils import slugify_instance_name
 
 
 # Create your models here.
 
 class Order(models.Model):
-   
     class Status(models.TextChoices):
         # Order status: 1 = Pending; 2 = Processing; 3 = Rejected; 4 = Completed
         PENDING = 'PENDING', 'Pending'
@@ -16,28 +14,34 @@ class Order(models.Model):
         COMPLETED = 'COMPLETED', 'Completed'
 
     status = models.CharField(max_length=20, default=Status.PROCESSING, choices=Status.choices)
-    created = models.DateTimeField(auto_now=True)
-    price = models.FloatField(blank=True)
-    required_date = models.DateTimeField(auto_now=True)
-    shipped_date = models.DateTimeField(auto_now_add=True)
+    price = models.FloatField(null=True, blank=True)
+
     doctor = models.ForeignKey(Doctor, related_name="doctor_orders", on_delete=models.CASCADE)
     delivery_worker = models.ForeignKey(DeliveryWorker, related_name="delivery_worker_orders", on_delete=models.SET_NULL, null=True)
+
+    # Flags
     accepted = models.BooleanField(default=False)
     submitted = models.BooleanField(default=False)
     delivered = models.BooleanField(default=False)
+
+    # Dates
+    created_at = models.DateTimeField(auto_now=True)
+    required_at = models.DateTimeField(auto_now=True)
+    shipped_date = models.DateTimeField(auto_now_add=True)
+
+    # Items
     items = models.ManyToManyField(Product, through='OrderProduct', related_name='orders')
 
     def __str__(self) -> str:
         return f"order-{self.id} by -> " + str(self.doctor)
 
-    def _calc_price(self):
-        # order_items = OrderItem.objects.filter(order=self)
-        # total = sum([each_item.price * each_item.consume_quantity for each_item in order_items])
-        return 1
+    # def _calc_total_price(self):
+    #     order_items = OrderProduct.objects.filter(order=self)
+    #     total = sum([each_item.price * each_item.consume_quantity for each_item in order_items])
+    #     return total
 
     
     def save(self, *args, **kwargs) -> None:
-        self.price = self._calc_price()
         return super().save(*args, **kwargs)
 
 
@@ -51,10 +55,10 @@ class Order(models.Model):
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=1)
     discount = models.FloatField(default=0)
-    price = models.FloatField(blank=True)
+    price = models.FloatField(null=True, blank=True)
     
 
     def _calc_price(self):
