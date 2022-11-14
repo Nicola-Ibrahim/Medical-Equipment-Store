9de730a120e1d581cc09b016ee648c503cf751ce
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-14 17:16:31 +0200

    accounts app signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,7 +9,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 
-from .models import User, Warehouse
+from .models import Warehouse
 from .permissions import PermissionGroupsName
 
 
