e742413090b87148cb7563b74b1488eff0f2f883
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-14 17:21:07 +0200

    warehouse app urls

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -7,7 +7,7 @@ app_name = 'warehouse'
 
 urlpatterns = [
     path("products_list/", ProductsListView.as_view(), name="products-list"),
-    path("product_details/<slug:slug>", ProductRetrieveView.as_view(), name="product-detail"),
+    path("product_details/<slug:slug>", ProductRetrieveView.as_view(), name="product-details"),
     path("orders_list/", WarehouseOrdersListView.as_view(), name="orders-list"),
     path("order_update/<int:pk>/", WarehouseOrdersUpdateView.as_view(), name="order-update"),
 ]
