d64ed7376c5642f278f6e427deb35df838ae9101
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-25 11:48:46 +0200

    Moved warehouse's orders queryset to the order app

--- product/mixins.py
+++ product/mixins.py
@@ -26,45 +26,12 @@ class ProductQuerySetMixin:
         return qs
     
 
-class WarehouseOrdersQuerySetMixin:
-    def get_queryset(self, *args, **kwargs):
-        """
-        Display only related orders to warehouse
-        Display all products if the user is admin
-        """
-        qs = Order.objects.none()
-
-        # If the user is admin
-        # Display all orders related to warehouses
-        if(self.request.user.type == User.Type.ADMIN):
-            qs = Order.objects.all()
-
-        # If the use is warehouse
-        # Display ONLY related orders
-        if(self.request.user.type == User.Type.WAREHOUSE):
-            lookup_filter = dict()
-            lookup_filter['warehouse'] = self.request.user
-            lookup_filter['product_set__isnull'] = False
-
-            lookup_values = ['name', 'product_set__order', 'product_set__order__price']
-
-
-            # Create INNER JOIN between Product model and OrderProduct model
-            products_qs = Product.objects.filter(**lookup_filter).values_list(*lookup_values)
-
-            # TODO: Customize the way of displaying the order 
-            # TODO: <QuerySet [('Tea1', 71, 4600.0), ('Coffee1', 71, 4600.0), ('Coffee1', 73, 1650.0)]>
-
-            order_ids = [item[1] for item in products_qs]
-            qs = Order.objects.filter(id__in=order_ids)
-        
-        return qs
 
 
 class ProductsSoldQuerySetMixin():
     def get_queryset(self, *args, **kwargs):
         """
-        Display the number of products sold for the warehouse
+        This QuerySet display the number of products sold
         """
         qs = Product.objects.none()
 
