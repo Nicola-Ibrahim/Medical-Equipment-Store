65fa1726f9a8e20fbd240c6c9a801011c238a438
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-25 11:46:58 +0200

    Moved the serializers and view that responsible for displaying warehouse coming orders to order app

--- product/serializers.py
+++ product/serializers.py
@@ -1,7 +1,5 @@
 from .models import Product
-from .services import send_msg
-from order.models import Order, OrderProduct
-from order.serializers import OrderProductsSerializer
+
 from accounts.models import Warehouse
 
 from rest_framework import serializers
@@ -9,7 +7,7 @@ from rest_framework import serializers
 class ProductSerializer(serializers.ModelSerializer):
 
     url = serializers.HyperlinkedIdentityField(
-        view_name='warehouse:product-details',
+        view_name='product:product-details',
         lookup_field = 'slug',
         read_only=True,
     )
@@ -37,56 +35,16 @@ class ProductSerializer(serializers.ModelSerializer):
     def to_internal_value(self, data):
         """Change data attributes value"""
 
-        # Connect the warehouse to current user
+        # Assign warehouse field to current user
         data['warehouse'] = self.context['request'].user.id
         
         return super().to_internal_value(data)
 
 
-
-class WarehouseOrdersSerializer(serializers.ModelSerializer):
-    """
-    This serializer only used to update order flags:
-    [accepted, submitted, delivered, rejected]
-
-    """
-
-    url = serializers.HyperlinkedIdentityField(
-        view_name='warehouse:order-update',
-        lookup_field='pk',
-        read_only=True
-    )
-
-    items = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Order
-        fields = [
-            'id', 
-            'url',
-            'price',
-            'status',
-            'accepted',
-            'submitted',
-            'delivered',
-            'rejected',
-            'items',
-            
-        ]
-        read_only_fields = ['id', 'url', 'price', 'status']
-
-    def get_items(self, obj):
-        """Custom Method-field to display related items in a specific shape"""
-        # Retrieve the sold items that relate to order 
-        qs = OrderProduct.objects.filter(order=obj)
-        return OrderProductsSerializer(qs, many=True, context=self.context).data
-
-    def update(self, instance, validated_data):
-        if(validated_data.get('accepted')):
-            send_msg('accepted')
-        return super().update(instance, validated_data)
-
 class ProductsSoldSerializer(serializers.Serializer):
+    """
+    This serializer handles the display of products sold to the warehouse
+    """
     name = serializers.CharField(read_only=True)
     sold_count = serializers.IntegerField(read_only=True)
 
@@ -96,3 +54,4 @@ class ProductsSoldSerializer(serializers.Serializer):
         qs = Warehouse.objects.get(id=obj['warehouse'])
         name = qs.warehouse_profile.name
         return name
+
--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, UpdateAPIView
 
 from .models import Product 
-from .serializers import ProductSerializer, WarehouseOrdersSerializer, ProductsSoldSerializer
-from .mixins import ProductQuerySetMixin, WarehouseOrdersQuerySetMixin, ProductsSoldQuerySetMixin
+from .serializers import ProductSerializer, ProductsSoldSerializer
+from .mixins import ProductQuerySetMixin, ProductsSoldQuerySetMixin
 
 
 from accounts.mixins import PermissionMixin
@@ -25,7 +25,7 @@ class ProductsListView(
     serializer_class = ProductSerializer
 
 
-class ProductRetrieveView(
+class ProductDetailsView(
     PermissionMixin,
     RetrieveUpdateDestroyAPIView):
 
@@ -36,38 +36,6 @@ class ProductRetrieveView(
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'slug'
-
-
-class WarehouseOrdersListView(
-    PermissionMixin,
-    WarehouseOrdersQuerySetMixin,
-    ListAPIView):
-
-    """
-    Obtain all related orders
-    -----
-    Make inner join between OrderProduct table 
-    and Product table where warehouse == current user
-    """
-    serializer_class = WarehouseOrdersSerializer
-
-    
-
-
-class WarehouseOrdersUpdateView(
-    PermissionMixin,
-    WarehouseOrdersQuerySetMixin,
-    UpdateAPIView
-    ):
-
-    """
-    Obtain all related orders
-    -----
-    Make inner join between OrderProduct table 
-    and Product table where warehouse == current user
-    """
-    serializer_class = WarehouseOrdersSerializer
-
 
 
 class ProductsSoldListView(
