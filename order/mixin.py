2fdddbf420f05d7b75ab1139dc060181628cc05c
Nicola Ibrahim <nicola123star4@gmail.com>
2022-11-27 12:18:58 +0200

    The queryset functionality when the use is a warehouse is changed

--- order/mixin.py
+++ order/mixin.py
@@ -1,18 +1,32 @@
 from accounts.models import User
 
 class OrderQuerySetMixin:
+    """
+    This queryset mixin displays orders
+    If the user is a admin then display all orders
+    If the user is a doctor then only display its orders
+    """
     def get_queryset(self, *args, **kwargs):
-        """
-        Display only related products to warehouse
-        Display all products if the user is admin
-        """
-        
+
+        # Order queryset
         qs = super().get_queryset(*args, **kwargs)
 
-        # Not admin user
-        if(self.request.user.type != User.Type.ADMIN):
-            lookup_data = dict()
-            lookup_data['doctor'] = self.request.user
-            qs = qs.filter(**lookup_data)
-        
+        match self.request.user.type:
+            case User.Type.DOCTOR:  # display its orders
+                lookup_data = dict()
+                lookup_data['doctor'] = self.request.user
+                qs = qs.filter(**lookup_data)
+            
+            case User.Type.WAREHOUSE:   # display ONLY related orders
+                lookup_filter = dict()
+                lookup_filter['product_set__isnull'] = False
+
+                warehouse_user = self.request.user
+
+                # Create INNER JOIN between Product model and OrderProduct model
+                order_ids = warehouse_user.products.filter(**lookup_filter).values_list('product_set__order', flat=True)
+
+                qs = qs.filter(id__in=set(order_ids))
+
+                
         return qs
