169a6c548d8ad4fe637a29c0dd30fff64f5a854a
Nicola Ibrahim <nicola123star4@gmail.com>
2022-12-02 21:30:34 +0200

    Added more filters on the products

--- product/filters.py
+++ product/filters.py
@@ -5,11 +5,20 @@ from .models import Product
 
 class ProductFilter(filters.FilterSet):
 
+
+    product_name = filters.CharFilter(field_name="name", lookup_expr="icontains")
+
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte') 
 
+    warehouse__warehouse_profile__name = filters.CharFilter(label='warehouse name', lookup_expr='icontains')
+    warehouse__warehouse_profile__sections__name = filters.CharFilter(label='section', lookup_expr='icontains')
+
+
     class Meta:
         model = Product
-        fields = {
-            "name": ['icontains', 'exact'], 
-            }
+
+        fields = [
+            
+        ]
+    
