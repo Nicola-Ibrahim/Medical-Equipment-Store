53769e04b28f177d189652c39afbb32b029d69dd
Nicola Ibrahim <nicola123star4@gmail.com>
2022-12-10 17:15:38 +0200

    Edited warehouse filter's fields

--- accounts/filters.py
+++ accounts/filters.py
@@ -5,12 +5,9 @@ from .models import Warehouse
 
 class WarehousesFilter(filters.FilterSet):
 
-    # min_price = filters.NumberFilter(field_name="total_price", lookup_expr='gte')
-    # max_price = filters.NumberFilter(field_name="total_price", lookup_expr='lte') 
-
     class Meta:
         model = Warehouse
         fields = {
             'warehouse_profile__name': ['icontains'], 
-            "warehouse_profile__service": ['icontains', 'exact'],
+            "warehouse_profile__sections__name": ['icontains', 'exact'],
             }
