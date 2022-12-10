b8737839663d1f2aa4175d065f9c7b7ae6c570ad
Nicola Ibrahim <nicola123star4@gmail.com>
2022-12-10 17:14:39 +0200

    Added Section and Service models serializers

--- accounts/profile_serializers.py
+++ accounts/profile_serializers.py
@@ -1,16 +1,55 @@
 from rest_framework import serializers
-from .profiles import WarehouseProfile, DoctorProfile, DeliveryWorkerProfile
+from .profiles import WarehouseProfile, DoctorProfile, DeliveryWorkerProfile, Section, Service
 
+class SectionSerializer(serializers.ModelSerializer):
+
+    # warehouses = WarehouseProfileSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Section
+        fields = ['name',]
+
+class ServiceSerializer(serializers.ModelSerializer):
+
+    # warehouses = WarehouseProfileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Service
+        fields = ['name',]
 
 class WarehouseProfileSerializer(serializers.ModelSerializer):
 
+    # sections = SectionSerializer(many=True)
+    # services = ServiceSerializer(many=True)
+
     class Meta:
         model = WarehouseProfile
-        fields = '__all__'
+        fields = ['name', 'sections', 'services', 'working_hours', 'profit_percentage', 'warehouse']
 
-        extra_kwargs = {
-            'warehouse': {'write_only': True},
-        }
+        # extra_kwargs = {
+        #     'warehouse': {'write_only': True},
+        # }
+
+    # def to_internal_value(self, data):
+    #     # print(data.getlist('sections')[:])
+    #     # print(data.getlist('services')[:])
+
+    #     # MultiValueDict
+
+    #     # print('-'*50)
+    #     data["sections"] = Section.objects.filter(id__in=data['sections']).values_list('id', flat=True)
+    #     data["services"] = Service.objects.filter(id__in=data['services']).values_list('id', flat=True)
+
+    #     print('to internal:', data)
+    #     print('to internal sections',data['sections'])
+    #     return super().to_internal_value(data)
+
+
+
+
+
+
+
 
 class DoctorProfileSerializer(serializers.ModelSerializer):
 
