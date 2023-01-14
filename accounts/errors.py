1f54349af46c0928494d6634aa02172ff64ba8bb
Nicola Ibrahim <nicolaibrahim969@gmail.com>
2023-01-14 22:00:33 +0200

    Added more errors handling classes

--- accounts/errors.py
+++ accounts/errors.py
@@ -5,9 +5,33 @@ class UserSerializerNotFound(exceptions.NotFound):
     """Exception class for not found the appropriate user serializer
 
     Args:
-        Exception (exceptions.NotFound): the serializer does not found
+        Exception (exceptions.NotFound): serializer does not found
     """
 
     def __init__(self, message=None):
         self.message = "Serializer not found" if not message else message
+        super().__init__(self.message)
+
+
+class UserFilterNotFound(exceptions.NotFound):
+    """Exception class for not found the appropriate user filter
+
+    Args:
+        Exception (exceptions.NotFound): filter does not found
+    """
+
+    def __init__(self, message=None):
+        self.message = "Filter not found" if not message else message
+        super().__init__(self.message)
+
+
+class UserModelNotFound(exceptions.NotFound):
+    """Exception class for not found the appropriate user filter
+
+    Args:
+        Exception (exceptions.NotFound): filter does not found
+    """
+
+    def __init__(self, message=None):
+        self.message = "Model not found" if not message else message
         super().__init__(self.message)
