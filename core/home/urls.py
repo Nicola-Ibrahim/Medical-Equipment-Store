17442bbe5c06bad285c99d7e1bc2a74bf0dc714b
Nicola Ibrahim <nicolaibrahim969@gmail.com>
2023-02-24 15:30:17 +0200

    project urls

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,18 +1,3 @@
-"""home URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,10 +8,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("core.apps.accounts.urls"), name='accounts'),
-    path("api/product/", include("core.apps.product.urls"), name='product'),
-    path("api/order/", include("core.apps.order.urls"), name='order'),
-    path("api/notification/", include("core.apps.notification.urls"), name='notification'),
+    path("api/accounts/", include("core.apps.accounts.urls"), name="accounts"),
+    path("api/product/", include("core.apps.product.urls"), name="product"),
+    path("api/order/", include("core.apps.order.urls"), name="order"),
+    path("api/notification/", include("core.apps.notification.urls"), name="notification"),
     # path("docs/", include_docs_urls(title='MedicalStorApi'), name=""),
     # path('api/schema/', get_schema_view(
     #     title="MedicalStorApi",
